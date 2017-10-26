27f0febe5306fbe7bd523eb26a61b360a470f2fd
jlb <bobo_66@hotmail.com>
2017-10-26 23:52:16 +0200

    12 questions Un seul bouton "nouvelle question" et "vérification" Pas de score

--- app/src/main/java/com/jlb/quizzcraft/MainActivity.java
+++ app/src/main/java/com/jlb/quizzcraft/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static com.jlb.quizzcraft.R.string.btn_play_question;
+
 public class MainActivity extends AppCompatActivity {
 
     QA currentQA = new QA();
@@ -17,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     private int reponse = 0;
 
     private Button btn_play = null;
-    private Button btn_validate = null;
     private TextView textview_question = null;
     private RadioGroup radiogroup_reponses = null;
     private RadioButton radiobtn_reponse_1 = null;
@@ -32,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_play = (Button) findViewById(R.id.button_start);
-        btn_validate = (Button) findViewById(R.id.button_validate);
         textview_question = (TextView) findViewById(R.id.textview_question);
         radiogroup_reponses = (RadioGroup) findViewById(R.id.radiogroup_reponses);
         radiobtn_reponse_1 = (RadioButton) findViewById(R.id.radiobtn_anwser_1);
@@ -41,74 +41,61 @@ public class MainActivity extends AppCompatActivity {
         radiobtn_reponse_4 = (RadioButton) findViewById(R.id.radiobtn_anwser_4);
         textview_result = (TextView) findViewById(R.id.textview_result);
 
-        // Désactive le bouton 'validation"
-        btn_validate.setEnabled(false);
-
-        // Listener du bouton 'Nouvelle partie'
+        // Listener du bouton 'play'
         btn_play.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                // Pose une question
-                currentQA = questionnaire.askQuestion();
-                textview_question.setText(currentQA.getQuestion());
-                radiobtn_reponse_1.setText(currentQA.getAnswer_1());
-                radiobtn_reponse_2.setText(currentQA.getAnswer_2());
-                radiobtn_reponse_3.setText(currentQA.getAnswer_3());
-                radiobtn_reponse_4.setText(currentQA.getAnswer_4());
+                if (btn_play.getText().equals(getResources().getString(R.string.btn_play_question))) {
+                    // Pose une question
+                    currentQA = questionnaire.askQuestion();
+                    textview_question.setText(currentQA.getQuestion());
+                    radiobtn_reponse_1.setText(currentQA.getAnswer_1());
+                    radiobtn_reponse_2.setText(currentQA.getAnswer_2());
+                    radiobtn_reponse_3.setText(currentQA.getAnswer_3());
+                    radiobtn_reponse_4.setText(currentQA.getAnswer_4());
 
-                // Dé-sélectionne les boutons radios
-                radiogroup_reponses.clearCheck();
+                    // Dé-sélectionne les boutons radios
+                    radiogroup_reponses.clearCheck();
 
-                // Ré-initialise le résultat
-                textview_result.setText("");
+                    // Ré-initialise le résultat
+                    textview_result.setText("");
 
-                // Désactive le bouton 'Nouvelle partie' et active 'validation'
-                btn_validate.setEnabled(true);
-                btn_play.setEnabled(false);
-            }
-        });
+                    // Change le texte du bouton
+                    btn_play.setText(R.string.btn_play_verify);
 
-        // Listener de bouton 'validation'
-        btn_validate.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                switch (radiogroup_reponses.getCheckedRadioButtonId()) {
-                    case R.id.radiobtn_anwser_1:
-                        reponse = 1;
-                        break;
-                    case R.id.radiobtn_anwser_2:
-                        reponse = 2;
-                        break;
-                    case R.id.radiobtn_anwser_3:
-                        reponse = 3;
-                        break;
-                    case R.id.radiobtn_anwser_4:
-                        reponse = 4;
-                        break;
-
-                    default:
-                        reponse = 0;
-                }
-                if (reponse == currentQA.getResult()) {
-                    textview_result.setTextColor(Color.GREEN);
-                    textview_result.setText("Félicitations, vous avez trouvé la bonne réponse !");
-
-                    // Active le bouton 'Nouvelle partie' et désactive 'validation"
-                    btn_validate.setEnabled(false);
-                    btn_play.setEnabled(true);
                 } else {
-                    textview_result.setTextColor(Color.RED);
-                    textview_result.setText("Désolé, ce n'était pas la bonne réponse !");
+                    switch (radiogroup_reponses.getCheckedRadioButtonId()) {
+                        case R.id.radiobtn_anwser_1:
+                            reponse = 1;
+                            break;
+                        case R.id.radiobtn_anwser_2:
+                            reponse = 2;
+                            break;
+                        case R.id.radiobtn_anwser_3:
+                            reponse = 3;
+                            break;
+                        case R.id.radiobtn_anwser_4:
+                            reponse = 4;
+                            break;
 
-                    // Désactive le bouton 'Nouvelle partie' et active 'validation'
-                    btn_validate.setEnabled(true);
-                    btn_play.setEnabled(false);
+                        default:
+                            reponse = 0;
+                    }
+                    if (reponse == currentQA.getResult()) {
+                        textview_result.setTextColor(Color.GREEN);
+                        textview_result.setText("Félicitations, vous avez trouvé la bonne réponse !");
+
+                        // Change le texte du bouton
+                        btn_play.setText(R.string.btn_play_question);
+
+                    } else {
+                        textview_result.setTextColor(Color.RED);
+                        textview_result.setText("Désolé, ce n'était pas la bonne réponse !");
+
+                    }
                 }
-
-
             }
         });
+
     }
-
-
 }
