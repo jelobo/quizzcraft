d02e96905e777c931f71b936aa59a559eb0dade8
jlb <bobo_66@hotmail.com>
2017-11-16 00:26:30 +0100

    47 questions avec images Ecran d'accueil Ecran Quizz 5 questions par partie Pop-up de fin avec score Conserve la liste des 10 dernières questions posées Info debug: n° question posée+solution+cycle de vie Menu: Musique + A propos + Niveau Musique, liée au cycle de vie Icône

--- app/src/main/java/com/jlb/quizzcraft/MainActivity.java
+++ app/src/main/java/com/jlb/quizzcraft/MainActivity.java
@@ -2,10 +2,10 @@ package com.jlb.quizzcraft;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,19 +14,22 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_new_game = null;
+    static public boolean mMusicEnabled = true;
+    static public Intent musicIntent = null;
 
+     // Cycle de vie
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MenuItem menuMusicEnabled = (MenuItem) findViewById(R.menu.menu_configuration);
+        Log.d("MainActivity", "onCreate");
 
         // Démarre la musique
-        startService(new Intent(this, MusicService.class));
+        musicIntent = new Intent(this, MusicService.class);
 
         // Init de la liste de questions
-        new ListOfQuestions();
+        new ListOfQuestions(0);
 
         btn_new_game = (Button) findViewById(R.id.btn_new_game);
 
@@ -34,16 +37,37 @@ public class MainActivity extends AppCompatActivity {
         btn_new_game.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                // Lance l'activité Quizz
+                 // Lance l'activité Quizz
                 Intent quizzActivityIntent = new Intent(MainActivity.this, QuizzActivity.class);
                 startActivity(quizzActivityIntent);
 
-                finish();
+                //finish();
             }
         });
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.d("MainActivity", "onResume");
+        if (mMusicEnabled) {
+            startService(musicIntent);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.d("MainActivity", "onPause");
+        if (mMusicEnabled ) {
+            stopService(musicIntent);
+        }
+    }
+
+    // Gestion du menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_configuration, menu);
@@ -51,21 +75,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Menu
+
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_level:
+            case R.id.menu_level_noob:
+                // Niveau de difficulté Noob
+                new ListOfQuestions(0);
+                item.setChecked(true);
+                return true;
+
+            case R.id.menu_level_average:
+                // Niveau de difficulté Moyen
+                new ListOfQuestions(1);
+                item.setChecked(true);
+                return true;
+
+            case R.id.menu_level_hardcore:
+                // Niveau de difficulté Hardcore
+                new ListOfQuestions(2);
+                item.setChecked(true);
                 return true;
 
             case R.id.menu_music_enabled:
                 if (item.isChecked()) {
                     // Met en pause la musique
-                    stopService(new Intent(this, MusicService.class));
+                     stopService(musicIntent);
 
+                    mMusicEnabled = false;
                     item.setChecked(false);
                 } else {
                     // Re-démarre la musique
-                    startService(new Intent(this, MusicService.class));
+                    startService(musicIntent);
 
+                    mMusicEnabled = true;
                     item.setChecked(true);
                 }
                 return true;
@@ -77,11 +119,12 @@ public class MainActivity extends AppCompatActivity {
                 builder.setMessage("Nombre de questions: " + ListOfQuestions.getNbOfQuestions() + "\nDéveloppeurs: Jelobo & Rombol");
                 builder.setCancelable(false);
 
-            // Bouton "OK"
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    dialog.cancel();
-                       }}).show();
+                // Bouton "OK"
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                }).show();
 
                 return true;
         }
@@ -90,3 +133,22 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
+//DONE En fin de partie, ajouter un bouton pour revenir au premier écran
+//TODO En fin de partie, afficher le nobmre de parties jouées et le total des points
+//TODO Emettre un son lorsque bonne ou mauvaise répoonse
+//TODO Ajouter des icônes dans les menus
+//DONE Suspendre la musisque lorsque l'application n'est plus active
+//TODO Libérer explicitement les ressources en sortie
+//TODO Support d'Android Froyo
+//TODO Adapter les images pour de petits écrans
+//TODO Ajouter un effet sur le bouton "Nouvelle partie"
+//TODO Modifier l'image du bouton "Nouvelle partie"
+//TODO Rémanence des options choisies
+//TODO Afficher la version de l'applications dans A propos
+//TODO Ajouter un contrôle du volume de la musique
+//TODO Décaler le démarrage de la musique pour réduire le ramp-up
+//TODO Supprimer les infos de debug du mode Release
+//TODO Ajouter une signature
+//TODO Publication sur le Google Play Store
+//TODO Localiser en anglais
+//TODO Commenter en javadoc
